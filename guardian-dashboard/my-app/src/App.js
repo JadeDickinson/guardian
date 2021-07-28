a6299b90cfbd91b11fc24e628c7ecbbe44fb258c
Jade Dickinson <14929975+JadeDickinson@users.noreply.github.com>
2021-07-28 09:45:19 +0100

    Create react app and add Victory library for dataviz

---
+++ guardian-dashboard/my-app/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as V from 'victory';
+import { VictoryBar } from 'victory';
+
+const data = [
+  {quarter: 1, earnings: 13000},
+  {quarter: 2, earnings: 16500},
+  {quarter: 3, earnings: 14250},
+  {quarter: 4, earnings: 19000}
+];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <VictoryBar
+          data={data}
+          // data accessor for x values
+          x="quarter"
+          // data accessor for y values
+          y="earnings"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
